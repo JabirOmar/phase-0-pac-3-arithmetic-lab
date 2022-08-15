562e910a54d212a9733072834b106142f3e5e93f
Jabir Omar <jabiromar400@gmail.com>
2022-08-15 19:40:20 +0300

    Completed Assingment

---
+++ index.js
@@ -0,0 +1,40 @@
+
+function add(a, b) {
+    return a + b
+}
+function subtract(a, b) {
+    return a - b   
+}
+function multiply(a, b) {
+    return a * b 
+}
+function divide(a, b) {
+    return a / b
+}
+
+function increment(n="53"){
+    n++
+    return n
+}
+function decrement(n="678"){
+    n--
+    return n
+}
+
+function makeInt(n='126'){
+    return parseInt(n)
+}
+
+function makeInt(n){
+    return parseInt(n, 10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+
+
+
+
+
+
+
